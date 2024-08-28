025cb2fc5fb5190c11b86e9d64a9384fad859fb2
Dingyelen <dingyanlin_happy@qq.com>
2024-08-28 15:04:25 +0800

    增加 DateFuncNew

---
+++ gantt_parser/untils/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: dingyelen
+# @Date:   2024-08-28 10:20:27
+# @Last Modified by:   dingyelen
+# @Last Modified time: 2024-08-28 14:33:30
+
+from .date_ranges import DateFuncNew
+
+__all__ = ['DateFuncNew']
